d3687b21119de75bcd5b94db280b502be43984c2
wuyunrui08 <49780726+wuyunrui08@users.noreply.github.com>
2019-12-02 21:27:21 +0800

    Create Cpp1.cpp

---
+++ StreamLearning/Cpp1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int ans;
+    char ch1;
+    cin>>ans>>ch1;
+    cout<<"ans= "<<ans<<endl<<" ch1= "<<ch1<<endl;
+	return 0;
+}
+
+//endl为输出回车换行项
